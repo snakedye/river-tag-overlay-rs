9dfe6127d64ddb4f00aa48986bb56c2839c10493
Bryan Ndjeutcha <ndjeutcha@gmail.com>
2021-08-14 07:50:41 -0400

    bad but alright

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,55 @@
 mod wayland;
+mod environment;
 
 use snui::*;
 use snui::widgets::*;
 use snui::wayland::app;
+use snui::wayland::app::Shell;
 
+const FG: u32 = 0xff_B5_B1_A4;
 const BG0: u32 = 0xff_26_25_25;
 const BG1: u32 = 0xff_33_32_32;
-const BG2: u32 = 0xff_40_3e_3e;
 const YEL: u32 = 0xff_c6_aa_82;
 const GRN: u32 = 0xff_98_96_7E;
 
-use smithay_client_toolkit::{
-    output::OutputHandler,
-    seat::SeatHandler,
-    shm::ShmHandler,
-    environment,
-    environment::{
-        Environment,
-        SimpleGlobal,
-    }
-};
-use wayland_client::protocol::{
-    wl_compositor::WlCompositor,
-    wl_output::WlOutput,
-    wl_seat::WlSeat,
-    wl_shm::WlShm,
-};
-use wayland_client::{Display, Proxy};
-use crate::wayland::river_status_unstable_v1::zriver_status_manager_v1::ZriverStatusManagerV1;
-use wayland_protocols::wlr::unstable::layer_shell::v1::client::zwlr_layer_shell_v1::ZwlrLayerShellV1;
+use environment::Environment;
+use wayland_client::{Display, Attached};
+use smithay_client_toolkit::shm::AutoMemPool;
 use wayland_protocols::wlr::unstable::layer_shell::v1::client::zwlr_layer_shell_v1::Layer;
 
 use crate::wayland::river_status_unstable_v1::zriver_output_status_v1;
-
-pub struct Env {
-    compositor: SimpleGlobal<WlCompositor>,
-    layer_shell: SimpleGlobal<ZwlrLayerShellV1>,
-    status_manager: SimpleGlobal<ZriverStatusManagerV1>,
-    shm: ShmHandler,
-    outputs: OutputHandler,
-    seats: SeatHandler,
-}
-
-impl Env {
-    fn new() -> Env {
-        Env {
-            compositor: SimpleGlobal::new(),
-            layer_shell: SimpleGlobal::new(),
-            status_manager: SimpleGlobal::new(),
-            shm: ShmHandler::new(),
-            outputs: OutputHandler::new(),
-            seats: SeatHandler::new()
-        }
-    }
-}
-
-environment!(Env,
-    singles = [
-        ZwlrLayerShellV1 => layer_shell,
-        ZriverStatusManagerV1 => status_manager,
-       	WlCompositor => compositor,
-       	WlShm => shm,
-    ],
-    multis=[
-        WlOutput => outputs,
-        WlSeat => seats,
-    ]
-);
+use crate::wayland::river_status_unstable_v1::zriver_seat_status_v1 ;
 
 fn main() {
     let display = Display::connect_to_env().unwrap();
     let mut event_queue = display.create_event_queue();
-    let wl_display = Proxy::clone(&display).attach(event_queue.token());
+    let environment = Environment::new(&display, &mut event_queue);
+    let attached = Attached::from(environment.shm.clone().expect("No shared memory pool"));
 
-    let env = Environment::new(&wl_display, &mut event_queue, Env::new()).unwrap();
-    let pointer = env.get_all_seats()[0].get_pointer();
-    app::quick_assign_pointer::<app::Application>(&pointer, None);
+    let layer_shell = environment.layer_shell.as_ref().unwrap();
+    let status_manager = environment.status_manager.as_ref().unwrap();
 
-    let pointer = env.get_all_seats()[0].get_pointer();
-    app::quick_assign_pointer::<app::Application>(&pointer, None);
+    let mut applications: Vec<app::Application> = environment.outputs.iter().enumerate().map(|(i, output)| {
+        let surface = environment.get_surface().detach();
+        let mempool = AutoMemPool::new(attached.clone()).unwrap();
+        let layer_surface = layer_shell
+            .get_layer_surface(&surface, Some(&output.wl_output), Layer::Top, String::from("overlay"));
 
-    let mempool = env.create_auto_pool();
-    let surface = env.create_surface().detach();
-    let layer_shell = env.require_global::<ZwlrLayerShellV1>();
-    let layer_surface = layer_shell
-        .get_layer_surface(&surface, None, Layer::Top, String::from("overlay"));
-    layer_surface.set_exclusive_zone(-1);
+        let mut app = app::Application::new(
+            create_widget(10, output.width as u32),
+            surface.clone(),
+            mempool,
+        );
+        app.attach_layer_surface(&layer_surface);
 
-    let mut applications = vec![app::Application::new(
-        create_widget(7),
-        surface.clone(),
-        mempool.unwrap(),
-    )];
-
-    applications[0].attach_layer_surface(&layer_surface);
-
-    let status_manager = env.require_global::<ZriverStatusManagerV1>();
-    for output in &env.get_all_outputs() {
         let mut tag_list = Vec::new();
-        let output_status = status_manager.get_river_output_status(&output);
+        let output_status = status_manager.get_river_output_status(&output.wl_output);
         output_status.quick_assign(move |_, event, mut applications | {
             match event {
                 zriver_output_status_v1::Event::FocusedTags { tags } => {
-                    let applications = applications.get::<Vec<app::Application>>().unwrap();
-                    for app in applications {
-                        app.widget.send_action(Action::Data("occupied", &tag_list));
-                        app.send_action(Action::Data("focused", &tags));
-                        break;
-                    }
+                    let application = &mut applications.get::<Vec<app::Application>>().unwrap()[i];
+                    application.dispatch(Command::Data("occupied", &tag_list));
+                	application.dispatch(Command::Data("focused", &tags));
                 }
                 zriver_output_status_v1::Event::ViewTags { tags } => {
                     let len = tags.len();
@@ -116,14 +58,14 @@ fn main() {
                             let buf: [u8; 4] = [tags[i], tags[i + 1], tags[i + 2], tags[i + 3]];
                             u32::from_le_bytes(buf)
                         }).collect();
-                        break;
                     }
                 }
             }
         });
-    }
+        app
+	}).collect();
 
-    loop {
+	loop {
         event_queue
             .dispatch(&mut applications, |event, object, _| {
                 panic!(
@@ -134,41 +76,63 @@ fn main() {
                 );
             })
             .unwrap();
-    }
+	}
 }
-pub fn create_widget(amount: u32) -> Border<Background<Rectangle, Wbox>> {
-    let hl = Rectangle::square(16, YEL);
 
-    let mut bar = Wbox::new(Orientation::Horizontal);
-    bar.set_spacing(5);
+fn run_command(value: String) {
+    use std::process::Command;
+    let mut string = value.split_whitespace();
+    let mut command = Command::new(string.next().unwrap());
+    command.args(string.collect::<Vec<&str>>());
+    command.spawn().expect("Error");
+}
+
+fn create_widget(amount: u32, width: u32) -> Border<Background<Rectangle, WidgetLayout>> {
+    let icon_size = 30;
+
+    let mut tags = WidgetLayout::new(Orientation::Horizontal);
+    tags.set_spacing(5);
 
     for n in 0..amount {
         let tag = 1 << n;
-        let icon = Background::new(Border::new(hl, 2, BG2), Rectangle::square(0,BG1), 15);
+        let icon = Background::new(Rectangle::square(20, BG1), Rectangle::square(0,BG1), icon_size - 4);
         let action = Actionnable::new(icon, move |icon, action| {
             if action.eq("focused") {
                 if let Some(focused) = action.get::<u32>() {
                     if focused == &tag || (focused / tag) % 2 != 0 {
-                        icon.widget.set_color(BG2);
-                        icon.widget.widget.set_color(YEL)
+                        icon.widget.set_color(YEL);
+                        return true
                     }
                 }
             } else if action.eq("occupied") {
                 if let Some(tags) = action.get::<Vec<u32>>() {
                     icon.widget.set_color(BG1);
-                    icon.widget.widget.set_color(BG1);
                     for t in tags {
                         if t == &tag {
-                            icon.widget.set_color(BG2);
-                            icon.widget.widget.set_color(GRN);
+                            icon.widget.set_color(GRN);
+                            return true
                         }
                     }
                 }
+            } else if action.eq("title") {
+                return true
             }
+            false
         });
-        bar.add(action).unwrap();
+        tags.add(Button::new(action, move |_, _, _, _, _, ev| match ev {
+	        pointer::Event::MouseClick {
+                time: _,
+                button: _,
+                pressed,
+            } => {
+                if pressed {
+                    run_command(format!("riverctl set-focused-tags {}", tag));
+                }
+                Damage::None
+            }
+            _ => Damage::None,
+        })).unwrap();
     }
 
-    bar.set_color(BG0);
-    boxed(bar, 5, 1, BG0, BG2)
+    boxed(tags, 5, 1, BG0, BG0)
 }
